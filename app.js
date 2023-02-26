1fd4c73586c2df81bc09d931be70415c4e80abe6
APIMontero <asia.david.montero@gmail.com>
2023-02-26 00:34:40 -0300

    Final Code Added

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const data = require("./src/js/alumnos.js");
 // defining the Express app
 const app = express();
 // defining an array to work as the database (temporary solution)
-const ads = alumnos.map((alumno) => alumno);
-
+let listadoAlumnos = alumnos.map((alumno) => alumno);
+// Configure port  to listen for this server 😎
+const PORT = process.env.PORT || 3000;
 // adding Helmet to enhance your Rest API's security
 app.use(helmet());
 
@@ -22,25 +23,26 @@ app.use(cors());
 // adding morgan to log HTTP requests
 app.use(morgan("combined"));
 
-// defining an endpoint to return all ads
-
+// defining an endpoint to return 'Alumnos' data
 app.get("/api/alumnos", (req, res) => {
-    res.send(ads);
+  res.send(listadoAlumnos);
 });
-// defining an endpoint to return all ads
+// defining an endpoint to return Instructions for API
 app.get("/", (req, res) => {
-    res.send(
-        '<h1>Intructiones de uso de la API</h1><ol><li>Escriba en la barra de navegación  localhost:5000/api/alumnos </li><li>Copie el contenido a otro archivo o úselo en su propia aplicación.</li></ol><p>Gracias por usa nuestra API.</p></div><div><a href="https://www.google.com/">Regresar a Internet</a></div>'
-    );
+  res.send(
+    `<h1>Intructiones de uso de la API</h1><ol><li>Escriba en la barra de navegación  localhost:${PORT}/api/alumnos </li><li>Copie el contenido a otro archivo o úselo en su propia aplicación.</li></ol><p>Gracias por usa nuestra API.</p></div><div><a href="https://www.google.com/">Regresar a Internet</a></div>`
+  );
 });
 app.get("/api", (req, res) => {
-    res.send(
-        '<h1>Intructiones de uso de la API</h1><ol><li>Escriba en la barra de navegación  localhost:5000/api/alumnos </li><li>Copie el contenido a otro archivo o úselo en su propia aplicación.</li></ol><p>Gracias por usa nuestra API.</p></div><div><a href="https://www.google.com/">Regresar a Internet</a></div>'
-    );
+  res.send(
+    `<h1>Intructiones de uso de la API</h1><ol><li>Escriba en la barra de navegación  localhost:${PORT}/api/alumnos </li><li>Copie el contenido a otro archivo o úselo en su propia aplicación.</li></ol><p>Gracias por usa nuestra API.</p></div><div><a href="https://www.google.com/">Regresar a Internet</a></div>`
+  );
 });
-//Config this data if you want!
-let port = process.env.PORT || 3000;
+
 // starting the server
-app.listen(port, () => {
-    console.log(`Listening on port ${port}\n(http://localhost:${port}/)`);
+app.listen(PORT, () => {
+  console.log(
+    `Listening on port ${PORT}\n\tAccess URL:\n\t\thttp://localhost:${PORT}\n\t\tAPI URI in http://localhost:${PORT}/api/alumnos`
+  );
+  console.log(`\t\tSe han encontrado ${listadoAlumnos.length} alumnos.\n`);
 });
